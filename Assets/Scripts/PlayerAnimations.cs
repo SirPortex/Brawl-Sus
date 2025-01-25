a344ebce5617bd3cb8b7e6ef7a93d0dd0c784617
Pavel.K144 <portex144@gmail.com>
2025-01-25 13:58:40 +0100

    PlayerInstantiate

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -1,22 +1,34 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimations : MonoBehaviour
+namespace Com.MyCompany.MyGame
 {
-    private Animator animator;
-    private PlayerMovement playerMovement;
-
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerAnimations : MonoBehaviourPun
     {
-        animator = GetComponent<Animator>();
-        playerMovement = GetComponent<PlayerMovement>();
-    }
+        private Animator animator;
+        private PlayerMovement playerMovement;
 
-    private void LateUpdate()
-    {
-        animator.SetFloat("Speed", playerMovement.GetCurrentSpeed() / playerMovement.walkingSpeed); //Variable de Speed del BlendTree que vaya relacionado con nuestra velocidada
+        // Start is called before the first frame update
+        void Start()
+        {
 
+            animator = GetComponent<Animator>();
+            playerMovement = GetComponent<PlayerMovement>();
+        }   
+
+
+        private void LateUpdate()
+        {
+            if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+            {
+                return;
+            }
+
+            animator.SetFloat("Speed", playerMovement.GetCurrentSpeed() / playerMovement.walkingSpeed); //Variable de Speed del BlendTree que vaya relacionado con nuestra velocidada
+
+        }
     }
 }
+
